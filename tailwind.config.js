7c24a0aa177e6736803b4f9d25496575084e6be4
amendezm <alexis.david.mendez@gmail.com>
2022-12-22 03:55:27 -0500

    styles: configure theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        skin: {
+          main: "var(--color-main-background)",
+          keypad: "var(--color-keypad-background)",
+          screen: "var(--color-screen-background)",
+          "key-primary": "var(--color-key-primary)",
+          "key-secondary": "var(--color-key-secondary)",
+          "key-muted": "var(--color-key-muted)",
+        },
+      },
+      textColor: {
+        skin: {
+          primary: "var(--color-text-primary)",
+          secondary: "var(--color-text-secondary)",
+        },
+      },
+    },
   },
   plugins: [],
 };
