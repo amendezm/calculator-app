1d09b316edf37e115b0f3813dc2b8bdb1e1667fb
amendezm <alexis.david.mendez@gmail.com>
2022-12-26 16:05:09 -0500

    feat: create screen component

--- components/calculator/Calculator.tsx
+++ components/calculator/Calculator.tsx
@@ -1,5 +1,11 @@
+import { Screen } from "./Screen";
 import { Keyboard } from "./Keyboard";
 
-const Calculator = () => <Keyboard />;
+const Calculator = () => (
+  <div className="flex flex-col">
+    <Screen />
+    <Keyboard />
+  </div>
+);
 
 export { Calculator };
---
+++ components/calculator/Screen.tsx
@@ -0,0 +1,7 @@
+const Screen = () => (
+  <div className="flex items-center justify-end bg-skin-screen w-full h-28 rounded-xl mb-6 p-4">
+    <span className="text-skin-switcher text-4xl">399,981</span>
+  </div>
+);
+
+export { Screen };
