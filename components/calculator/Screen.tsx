53db07a291868cc744d0562c49163d4693bacd80
amendezm <alexis.david.mendez@gmail.com>
2022-12-27 00:37:49 -0500

    feat: allow calculate expression

--- components/calculator/Screen.tsx
+++ components/calculator/Screen.tsx
@@ -3,10 +3,13 @@ import { Textfit } from "react-textfit";
 import { useCalculatorContext } from "context";
 
 const Screen = () => {
-  const { value } = useCalculatorContext();
+  const { value, expression } = useCalculatorContext();
 
   return (
-    <div className="flex items-center justify-end bg-skin-screen w-full h-28 sm:h-32 rounded-xl mb-6 p-4">
+    <div className="relative flex items-center justify-end bg-skin-screen w-full h-28 sm:h-32 rounded-xl mb-6 p-4">
+      <span className="absolute top-2 right-2 text-skin-switcher text-right text-xs opacity-60">
+        {expression}
+      </span>
       <Textfit
         className="text-skin-switcher text-right text-5xl w-full"
         mode="single"
--- context/CalculatorProvider.tsx
+++ context/CalculatorProvider.tsx
@@ -16,6 +16,8 @@ interface ICalculatorProviderProps {
   children: ReactNode;
 }
 
+const operators = ["+", "-", "/", "x"];
+
 const CalculatorProvider: FC<ICalculatorProviderProps> = ({ children }) => {
   const [expression, setExpression] = useState("");
   const [value, setValue] = useState("0");
@@ -26,12 +28,33 @@ const CalculatorProvider: FC<ICalculatorProviderProps> = ({ children }) => {
       case "-":
       case "x":
       case "/":
+        if (!expression.length) {
+          setExpression(`${value}${keyValue}`);
+          return;
+        }
+        if (operators.includes(expression.at(-1))) {
+          setExpression(
+            (expression) => `${expression.slice(0, -1)}${keyValue}`
+          );
+          return;
+        }
+        setExpression((expression) => `${expression}${keyValue}`);
         return;
       case ".":
         if (value.includes(".")) return;
         setValue((value) => `${value}.`);
         return;
       case "=":
+        if (!expression.length) {
+          return;
+        }
+        if (operators.includes(expression.at(-1))) {
+          setValue(`${eval(expression.slice(0, -1))}`);
+          setExpression("");
+          return;
+        }
+        setValue(`${eval(expression)}`);
+        setExpression("");
         return;
       case "del":
         if (value.length === 1) {
@@ -47,6 +70,7 @@ const CalculatorProvider: FC<ICalculatorProviderProps> = ({ children }) => {
         });
         return;
       case "reset":
+        setExpression("");
         setValue("0");
         return;
       case "0":
@@ -54,6 +78,12 @@ const CalculatorProvider: FC<ICalculatorProviderProps> = ({ children }) => {
           return;
         }
       default:
+        if (countOperators(expression) > 0) {
+          const newExpression = `${expression}${keyValue}`.replace(/x/g, "*");
+          setExpression((expression) => `${expression}${keyValue}`);
+          setValue(`${eval(newExpression)}`);
+          return;
+        }
         const numericValue = value.replace(/,/g, "");
         if (numericValue.length === 15) return;
 
@@ -67,6 +97,14 @@ const CalculatorProvider: FC<ICalculatorProviderProps> = ({ children }) => {
     </CalculatorContext.Provider>
   );
 };
+
+const countOperators = (expression: string) =>
+  expression
+    .split("")
+    .reduce(
+      (acc, operator) => (operators.includes(operator) ? acc + 1 : acc),
+      0
+    );
 
 const useCalculatorContext = () => useContext(CalculatorContext);
 
